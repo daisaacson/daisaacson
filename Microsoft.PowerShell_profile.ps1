d213c54e74ec33fbf218e97cc246ea5ccbe855d4
daisaacson <49328302+daisaacson@users.noreply.github.com>
2021-12-17 15:46:12 -0600

    add pwsh profile

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,16 @@
+if ( Get-Command Set-PSReadlineKeyHandler -ErrorAction SilentlyContinue ) {
+	Set-PSReadlineKeyHandler -Key Tab -Function Complete
+	Set-PSReadlineKeyHandler -key ctrl+d -Function ViExit
+}
+
+Set-Alias -Name which -Value Get-Command
+
+Set-PSReadLineOption -PredictionSource History
+
+Set-PoshPrompt -Theme half-life
+Import-Module -Name Terminal-Icons
+<#
+function prompt {
+    "[$($env:Username)@$($env:COMPUTERNAME) $(Split-Path (Get-Location) -Leaf)]> "
+}
+#>
