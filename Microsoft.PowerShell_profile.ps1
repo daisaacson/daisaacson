5fc22ff743b69d96270fb14f5f68544f00cd1b47
daisaacson <49328302+daisaacson@users.noreply.github.com>
2022-07-15 21:40:40 -0500

    update pwsh porfile

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,11 +1,19 @@
 if ( Get-Command Set-PSReadlineKeyHandler -ErrorAction SilentlyContinue ) {
-	Set-PSReadlineKeyHandler -Key Tab -Function Complete
-	Set-PSReadlineKeyHandler -key ctrl+d -Function ViExit
+	Set-PSReadLineKeyHandler -Chord Tab    -Function Complete
+	Set-PSReadLineKeyHandler -Chord Ctrl+a -Function BeginningOfLine
+	Set-PSReadLineKeyHandler -Chord Ctrl+b -Function BackwardChar
+	Set-PSReadLineKeyHandler -Chord  Alt+b -Function BackwardWord
+	Set-PSReadLineKeyHandler -Chord Ctrl+d -Function ViExit
+	Set-PSReadLineKeyHandler -Chord Ctrl+e -Function EndOfLine
+	Set-PSReadLineKeyHandler -Chord Ctrl+f -Function ForwardChar
+	Set-PSReadLineKeyHandler -Chord  Alt+f -Function ForwardWord
+	Set-PSReadLineKeyHandler -Chord Ctrl+t -Function SwapCharacters
 }
 
+Set-Alias -Name g     -Value git.exe
+Set-Alias -Name jq    -Value ConvertTo-Json
+Set-Alias -Name k     -Value kubectl.exe
 Set-Alias -Name which -Value Get-Command
 
-Set-PSReadLineOption -PredictionSource History
-
-Set-PoshPrompt -Theme half-life
+oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\half-life.omp.json" | Invoke-Expression
 Import-Module -Name Terminal-Icons
